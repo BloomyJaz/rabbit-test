38358381e64f65e69ba1f814b535a1d8a021f52b
Bloomyjaz <89125709+BloomyJaz@users.noreply.github.com>
2023-01-12 11:02:09 -0700

    Add files via upload

---
+++ rabbits.py
@@ -0,0 +1,16 @@
+"""
+Rabbits
+By: BloomyJaz
+"""
+
+
+
+#sets 'rabbits' to 20
+rabbits = 20
+
+#prints "There are 20 rabbits!" by concating the string and converting the integer rabbits to a string
+print("There are " + str(rabbits) + " rabbits!")
+print("Wowee!!")
+print()
+
+end = input("Press enter to leave.")
